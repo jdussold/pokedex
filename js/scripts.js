4e199ecf727361505d569a3d6ff3a34522794e59
jdussold <jdussold@gmail.com>
2022-09-10 01:13:22 -0500

    defined pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [{name: 'Charizard', height: 6, types: ['Fire','Flying']},{name: 'Raichu', height: 3, types:['Electric','Psychic']},{name: 'Mimikyu', height: 1, types: ['Ghost','Fairy']}];
