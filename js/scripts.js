e23663b3cffecde6884ead2e08a71c12da166592
jdussold <jdussold@gmail.com>
2022-09-10 01:19:09 -0500

    reformatted with prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [{name: 'Charizard', height: 6, types: ['Fire','Flying']},{name: 'Raichu', height: 3, types:['Electric','Psychic']},{name: 'Mimikyu', height: 1, types: ['Ghost','Fairy']}];
+let pokemonList = [
+  { name: "Charizard", height: 6, types: ["Fire", "Flying"] },
+  { name: "Raichu", height: 3, types: ["Electric", "Psychic"] },
+  { name: "Mimikyu", height: 1, types: ["Ghost", "Fairy"] },
+];
