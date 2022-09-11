988f0672d885e7f3e4029c55aa61123a205e8115
jdussold <jdussold@gmail.com>
2022-09-11 17:10:47 -0500

    updated with header and selected font

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@ let pokemonList = [
   { name: "Raichu", height: 3, types: ["Electric", "Psychic"] },
   { name: "Mimikyu", height: 1, types: ["Ghost", "Fairy"] },
 ];
+
+//for loop to write out the full list of pokemon defined thus far and to check for certain heights
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 3) {
+    document.write(
+      pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ")" +
+        " - Wow, that's big!<br>"
+    );
+  } else {
+    document.write(
+      pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>"
+    );
+  }
+}
