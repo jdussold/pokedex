a2ab3e5de353df4140947a4d05befc4f609009e4
jdussold <jdussold@gmail.com>
2022-09-13 23:04:09 -0500

    added IIFE and updated forEach to work with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-let pokemonList = [
-  { name: "Charizard", height: 6, types: ["Fire", "Flying"] },
-  { name: "Raichu", height: 3, types: ["Electric", "Psychic"] },
-  { name: "Mimikyu", height: 1, types: ["Ghost", "Fairy"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Charizard", height: 6, types: ["Fire", "Flying"] },
+    { name: "Raichu", height: 3, types: ["Electric", "Psychic"] },
+    { name: "Mimikyu", height: 1, types: ["Ghost", "Fairy"] },
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll(pokemon) {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
 //forEach function to write out the full list of pokemon defined thus far and call out specific pokemon over a certain height
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   if (pokemon.height > 3) {
     document.write(
       pokemon.name +
