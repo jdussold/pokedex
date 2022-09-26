b469a6f291b3d6ddc097835a9ee8c25ff5a0aff3
jdussold <jdussold@gmail.com>
2022-09-25 21:27:28 -0500

    added   addListItem() and updated notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,58 @@
+//defining the initial pokemon repository list
 let pokemonRepository = (function () {
-  let pokemonList = [
+  let repository = [
     { name: "Charizard", height: 6, types: ["Fire", "Flying"] },
     { name: "Raichu", height: 3, types: ["Electric", "Psychic"] },
     { name: "Mimikyu", height: 1, types: ["Ghost", "Fairy"] },
+    { name: "Dialga", height: 17, types: ["Steel", "Dragon"] },
+    { name: "Braviary", height: 5, types: ["Normal", "Flying"] },
   ];
 
+  //defines function to add new pokemon to list, as long as each new pokemon has a "name", "height", and "types" defined.
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
 
-  function getAll(pokemon) {
-    return pokemonList;
+  //defines function to return entire pokemon repository
+  function getAll() {
+    return repository;
+  }
+
+  //defines function to log details for pokemon that is clicked
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  //defines function to add new items/buttons to page.  This takes all the pokemon from the repository and creates a new "li" element and button.  Buttons are created using the pokemon name as their innerText.  Lastly an eventListener is added to listen for mouse clicks and log the details in the console.
+  function addListItem(pokemon) {
+    let listOfPokemon = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("pokemonButton");
+    listItem.appendChild(button);
+    listOfPokemon.appendChild(listItem);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
   }
 
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
-//forEach function to write out the full list of pokemon defined thus far and call out specific pokemon over a certain height
 pokemonRepository.getAll().forEach(function (pokemon) {
-  if (pokemon.height > 3) {
-    document.write(
-      pokemon.name +
-        " (height: " +
-        pokemon.height +
-        ")" +
-        " - Wow, that's big!<br>"
-    );
-  } else {
-    document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
-  }
+  pokemonRepository.addListItem(pokemon);
 });
