a627635fe463ef44b824744989350ecb218c27ae
jdussold <jdussold@gmail.com>
2022-10-06 23:14:59 -0500

    added modal script

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 const pokemonRepository = (function () {
   const pokemonList = [];
   const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  const pokemonListElement = document.querySelector(".pokemon-list");
 
   //defines function to add new pokemon to list and performs some basic validation
   function add(pokemon) {
@@ -76,10 +77,62 @@ const pokemonRepository = (function () {
 
   //defines a function to log details (image url, height, and types) for pokemon that is clicked.  This will be added to the bottom of the console log.  Reminder that the console log is pre-populated with all the names and detailsURL for all pokemon in our list due to our "loadList" function above.
   function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      showModal(pokemon);
     });
   }
+
+  function showModal(pokemon) {
+    const modalContainer = document.querySelector("#modal-container");
+    //clear existing content
+    modalContainer.innerHTML = "";
+
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    //add close button to modal
+    const closeButtonElement = document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText = "Close";
+    closeButtonElement.addEventListener("click", hideModal);
+
+    //add content to modal
+    const pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemon.imageUrl;
+
+    const pokemonName = document.createElement("h1");
+    pokemonName.innerText = pokemon.name;
+
+    const pokemonHeight = document.createElement("p");
+    pokemonHeight.innerText = `Height: ${pokemon.height}`;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(pokemonName);
+    modal.appendChild(pokemonImage);
+    modal.appendChild(pokemonHeight);
+    modalContainer.appendChild(modal);
+
+    modalContainer.addEventListener("click", (e) => {
+      const target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+    modalContainer.classList.add("is-visible");
+  }
+
+  function hideModal() {
+    const modalContainer = document.querySelector("#modal-container");
+    modalContainer.classList.remove("is-visible");
+  }
+
+  //hide modal if ESC key is pressed
+  window.addEventListener("keydown", (e) => {
+    const modalContainer = document.querySelector("#modal-container");
+    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+      hideModal();
+    }
+  });
 
   //The pokemonRepository is defined and housed within an IIFE (Immediately invoked function expression).  This way, you can't directly access it from the outside, which is what protects it.  This makes it nearly impossible to accidentally modify(or access) the shared state, since you always need to explicitly access it via the repository's exposed functions.  The IIFE then returns an object with the following keys.  This means whenever you access the pokemonRepository somewhere in the app, it will represent an object with these keys.
   return {
